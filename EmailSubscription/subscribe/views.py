755576cf9a99746b3828344832d1c59a7fed55c3
rajesh-palai <rajeshpalai303@gmail.com>
2020-01-25 15:06:34 +0530

    1st commit

---
+++ EmailSubscription/subscribe/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from EmailSubscription.settings import EMAIL_HOST_USER
+from . import forms
+from django.core.mail import send_mail
+
+
+# Create your views here.
+def subscribe(request):
+    sub = forms.Subscribe()
+    if request.method == 'POST':
+        sub = forms.Subscribe(request.POST)
+        subject = 'Welcome friend'
+        message = 'Hope you are enjoying my django project'
+        recepient = str(sub['Email'].value())
+        send_mail(subject, message, EMAIL_HOST_USER, [recepient], fail_silently=False)
+        return render(request, 'subscribe/success.html', {'recepient': recepient})
+
+    return render(request, 'subscribe/index.html', {'form': sub})
+
+
